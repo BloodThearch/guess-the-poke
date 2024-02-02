b0451a460ddb66cd497024921c562258c224925d
BloodThearch <gautamjain2323@gmail.com>
2024-02-02 14:55:03 +0530

    dev: reformatting

--- server.js
+++ server.js
@@ -2,24 +2,24 @@ const cluster = require("cluster")
 const os = require('node:os')
 const process = require('node:process');
 
-const {createApp} = require("./index")
+const { createApp } = require("./index")
 
 const numCPUs = os.availableParallelism()
 
 if (cluster.isPrimary) {
     console.log(`Available CPUs: ${numCPUs}`)
     console.log(`Primary ${process.pid} is running`);
-  
+
     // Fork workers.
     for (let i = 0; i < numCPUs; i++) {
-      cluster.fork();
+        cluster.fork();
     }
-  
+
     cluster.on('exit', (worker, code, signal) => {
-      console.log(`worker ${worker.process.pid} died`);
+        console.log(`worker ${worker.process.pid} died`);
     });
-  } else {
+} else {
     createApp()
-  
+
     console.log(`Worker ${process.pid} started`);
-  }
+}
